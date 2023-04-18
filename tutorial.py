90c249876462d618c5557a3878b0db5249655e34
with2say <with2say@gmail.com>
2023-04-19 01:22:34 +0900

    Revise tutorial

--- tutorial.py
+++ tutorial.py
@@ -25,7 +25,7 @@ def main(n_samples=10000, n_epoch=200):
     print('null mse:', np.var(targets))
     print(data.shape, targets.shape)
 
-    data_module = PolygonAreaDataModule(data, targets, batch_size=512, val_split=0.1, test_split=0.1, num_workers=4)
+    data_module = PolygonAreaDataModule(data, targets, batch_size=512, val_split=0.1, test_split=0.1, num_workers=2)
 
     # MultiShapeEmbedding 객체 생성
     d_model = 8
